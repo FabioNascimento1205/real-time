1370e2e18a4ac0d9fdfa2c98fb660dc28264f9cf
Fabio Nascimento <fab.nascimento1205@gmail.com>
2023-01-12 21:03:42 -0300

    version3.0

---
+++ script/app.js
@@ -0,0 +1,35 @@
+const relogio = setInterval(function time(){
+    const hora = document.querySelector('span#hora');
+    const min = document.querySelector('span#minuto');
+    const seg = document.querySelector('span#segundo');
+    const img = document.querySelector('img#imagem');
+    const msg = document.querySelector('header#saudacao');
+
+    let dateDay = new Date();
+    let hr = dateDay.getHours();
+    let mn = dateDay.getMinutes();
+    let sg = dateDay.getSeconds();
+
+    /* condição para os zeros aparecer no relogio */
+
+    if(hr<10) hr = '0' + hr;
+    if(mn<10) mn = '0' + mn;
+    if(sg<10) sg = '0' + sg;
+
+    /* Evento utilizada para fazer o relogio funcionar */
+
+    hora.textContent = hr;
+    min.textContent = mn;
+    seg.textContent = sg;
+
+    if(hr >=5 && hr <12){
+        msg.innerHTML = 'BOM DIA'
+        img.src = './img/manha.png'
+    }else if(hr >=12 && hr <18){
+        msg.innerHTML = 'BOA TARDE'
+        img.src = './img/tarde.png'
+    }else{
+        msg.innerHTML = 'BOA NOITE'
+        img.src = './img/noite.png'
+    }
+})
